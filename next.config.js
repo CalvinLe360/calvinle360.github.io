c5a5a0598ad5b5463e705ee80ba1e5d30ffd21c3
Snoresville <calvinle360@gmail.com>
2023-01-31 21:12:54 +1030

    Next.JS deployment is rough

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ const isProd = process.env.NODE_ENV === "production";
 module.exports = {
     assetPrefix: isProd ? "/calvinle360.github.io/" : "",
     images: {
-        unoptimized: true,
+        loader: "akamai",
     },
 };
