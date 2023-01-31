c1373ddef1342585051a5fa250795ecbaec7cfb2
Snoresville <calvinle360@gmail.com>
2023-01-31 20:45:59 +1030

    Testing to see if the images will reappear

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     reactStrictMode: true,
     images: {
-        loader: "akamai",
+        unoptimized: true,
         domains: [
             "cdn-images-1.medium.com",
             "miro.medium.com",
