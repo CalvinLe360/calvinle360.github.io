fadd473bb9a07fd0ef0fa5151a64c92a251d0081
Snoresville <calvinle360@gmail.com>
2023-01-31 22:29:30 +1030

    committing

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
     images: {
-        loader: "akamai",
+        domains: [
+            "cdn-images-1.medium.com",
+            "miro.medium.com",
+            "res.cloudinary.com",
+        ],
     },
 };
