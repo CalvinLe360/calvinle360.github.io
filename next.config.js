96d44a61eaa2b1abea055e2ed94ab8e85bb64373
Snoresville <calvinle360@gmail.com>
2023-02-02 00:12:05 +1030

    Revert "here we go again..."
    This reverts commit 1ade722cf51c526d969bd9e19c5746ebba61885a.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 module.exports = {
-    reactStrictMode: true,
-    images: {
-        loader: "akamai",
-        domains: [
-            "cdn-images-1.medium.com",
-            "miro.medium.com",
-            "res.cloudinary.com",
-        ],
-    },
-};
+  reactStrictMode: true,
+  images: {
+    domains: ['cdn-images-1.medium.com', 'miro.medium.com', 'res.cloudinary.com'],
+  },
+}
