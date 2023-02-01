60336fc64ecb361ea20a324987ec40420792e4f3
Snoresville <calvinle360@gmail.com>
2023-02-01 18:36:26 +1030

    CSS Experimentation

--- Constants/projects.js
+++ Constants/projects.js
@@ -47,7 +47,7 @@ export const projects = [
     {
         name: "My Portfolio Website",
         description:
-            "Any project in any discipline will always come with struggles and frustration, but the end result will always wrap up the experience with gratification.",
+            "Any project in any discipline will always come with struggles and frustration, but the end result will always wrap up the experience with gratification. With much appreciation to Asavari Ambavane for developing the template for the website.",
         githubLink: "https://github.com/CalvinLe360/calvinle360.github.io",
         projectLink: "/",
         tech: ["Next.JS", "CSS"],
